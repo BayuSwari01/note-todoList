d73ee14ba000ee0a0464388b6fdb70d3d0f283c7
BayuSwari01 <bayuswari.01@gmail.com>
2022-10-22 11:55:36 +0700

    menambahkan scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,10 @@
     <div class="container text-center">
         <div class="row align-items-start">
             <div class="col">
-                One of three columns
+                <div class="isi"><h1>ayo catat</h1></div>
             </div>
             <div class="col">
-                <img src="{{ asset('gambar-note.png') }}" alt="">
+                <img src="{{ asset('images/gambar-note.png') }}" alt="">
             </div>
         </div>
     </div>
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -5,28 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
   <body>
+    <div class="utama" style="display: flex; flex-direction: column; min-height: 100vh">
       {{-- bagian nav bar --}}
       @section('navbar')
-      <nav class="navbar navbar-expand-lg " style="background-color: #2f302f">
+      <nav class="navbar navbar-expand-lg bg-primary">
         <div class="row container-fluid">
           <div class="col-9 text-start text-white">
-            <h2 style="color: #8bb158">NoteList</h2>
+            <h2 class="text-light">NoteList</h2>
           </div>
           <div class="col-3 justify-content-end">
-            <button class="btn btn-outline-success position-absolute top-50 end-0 translate-middle-y" type="submit" style="color: #8bb158; margin-right: 10px">Login</button>
+            <button class="btn btn-outline-light position-absolute top-50 end-0 translate-middle-y" type="submit" style="margin-right: 10px">Login</button>
           </div>
         </div>
       </nav>
       @show
 
       {{-- body page --}}
-      <div class="container-fluid text-start" style="background-color: #424242; height: 1000px">
+      <div class="container-fluid text-start bg-secondary" style="flex: 1 1 0%;">
         @yield('content')
       </div>
+    </div>
+      
     
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
